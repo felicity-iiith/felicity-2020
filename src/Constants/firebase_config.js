225599973ed5f88f11c7b7f2d22b91ef472d5f99
alti-tude <kartik.gupta0204@gmail.com>
2019-10-29 19:11:45 +0530

    updated package.json for gallery, added common firebase config

---
+++ src/Constants/firebase_config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzk0rEn1JV1S_t_cD1COAV1dxX7xi2rJo",
+    authDomain: "felicitytest-6c60c.firebaseapp.com",
+    databaseURL: "https://felicitytest-6c60c.firebaseio.com",
+    projectId: "felicitytest-6c60c",
+    storageBucket: "felicitytest-6c60c.appspot.com",
+    messagingSenderId: "860737957179",
+    appId: "1:860737957179:web:e013d5f95714586b8fdb1e",
+    measurementId: "G-W06ST8Y9KV"
+  };
