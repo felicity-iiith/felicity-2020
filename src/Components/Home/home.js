2bed4fd398e2ba5b2c910b6ca047f8b05d8e8dfe
alti-tude <kartik.gupta0204@gmail.com>
2019-10-29 15:42:03 +0530

    added unprotected route for admin, localstorage protection for home

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import * as ROUTES from '../../Constants/routes';
+
 // import { compose } from 'recompose';
 
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-    
-  </div>
-);
+const HomePage = () => {
+  if(localStorage.getItem('authuser')!=null)
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
+        
+      </div>
+    );
+  else window.location = ROUTES.SIGN_IN;
+};
 
 export default HomePage;
--- src/Constants/routes.js
+++ src/Constants/routes.js
@@ -2,4 +2,5 @@ export const LANDING = '/';
 export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
 
