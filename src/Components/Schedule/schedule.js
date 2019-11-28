bfe5d5cdc45a0a3b3b8f4eeef153f274c118ddd3
Vipul Chhabra <vipulchhabra555@gmail.com>
2019-11-28 07:56:15 +0530

    schedule page completed

--- src/Components/Schedule/schedule.js
+++ src/Components/Schedule/schedule.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+//import { Icon } from 'react-native-elements';
+import UseAnimations from 'react-useanimations';
+const back = {
+  background : '#29ffff',
+};
 
+const App = () => <UseAnimations animationKey="github" />;
 
 const Schedule = () => (
- 
-<VerticalTimeline>
+
+
+    <div style = {back}>
+      <VerticalTimeline>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    //contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
     date="2011 - present"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
+    icon={App}
   >
     <h3 className="vertical-timeline-element-title">Creative Director</h3>
     <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -24,7 +32,7 @@ const Schedule = () => (
     className="vertical-timeline-element--work"
     date="2010 - 2011"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
+    //icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">Art Director</h3>
     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -36,7 +44,7 @@ const Schedule = () => (
     className="vertical-timeline-element--work"
     date="2008 - 2010"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
+    //icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">Web Designer</h3>
     <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
@@ -48,7 +56,7 @@ const Schedule = () => (
     className="vertical-timeline-element--work"
     date="2006 - 2008"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
+    //icon={<WorkIcon />}
   >
     <h3 className="vertical-timeline-element-title">Web Designer</h3>
     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -60,7 +68,7 @@ const Schedule = () => (
     className="vertical-timeline-element--education"
     date="April 2013"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
+    //icon={<SchoolIcon />}
   >
     <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
     <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
@@ -72,7 +80,7 @@ const Schedule = () => (
     className="vertical-timeline-element--education"
     date="November 2012"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
+    //icon={<SchoolIcon />}
   >
     <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
     <h4 className="vertical-timeline-element-subtitle">Certification</h4>
@@ -84,6 +92,7 @@ const Schedule = () => (
     className="vertical-timeline-element--education"
     date="2002 - 2006"
     iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    //icon={<SchoolIcon />}
   >
     <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
     <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
@@ -93,10 +102,11 @@ const Schedule = () => (
   </VerticalTimelineElement>
   <VerticalTimelineElement
     iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-
+    //icon={<StarIcon />}
   />
 </VerticalTimeline>
-  
+    </div>
+
 
 );
   
