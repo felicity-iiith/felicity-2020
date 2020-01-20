3983e3286ac80b16a810af474c5f4d673089dcb0
Shaunak <shaunak.badani@research.iiit.ac.in>
2020-01-21 04:14:56 +0530

    First version of website up

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navigation from '../Navigation';
-import LandingPage from '../Landing';
+// import Navigation from '../Navigation';
+// import LandingPage from '../Landing';
 import SignInPage from '../SignIn';
-import HomePage from '../Home';
+// import HomePage from '../Home';
 import GalleryPage from '../Gallery';
 import NewUser from '../NewUser';
 // import AccountPage from '../Account';
@@ -17,6 +17,9 @@ import Home from '../TestHome';
 import Sponsors from '../TestHome/Sponsors/Sponsors';
 import Contact from '../TestHome/Contact/Contact';
 
+
+// SCSS FILES
+
 const App = () => (
   <Router>
     <div>
@@ -24,12 +27,12 @@ const App = () => (
 
 
         <Route exact path={ROUTES.LANDING} component={Home} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.GALLERY} component={GalleryPage} />
+        {/* <Route path={ROUTES.SIGN_IN} component={SignInPage} /> */}
+        {/* <Route path={ROUTES.GALLERY} component={GalleryPage} /> */}
         <Route path={ROUTES.SPONSORS} component={Sponsors} />
         <Route path={ROUTES.CONTACT} component={Contact} />
-        <Route path={ROUTES.ADMIN} component={Admin} />
-        <Route path={ROUTES.ADMINQUIZ} component={AdminQuiz} />
+        {/* <Route path={ROUTES.ADMIN} component={Admin} /> */}
+        {/* <Route path={ROUTES.ADMINQUIZ} component={AdminQuiz} /> */}
         <Route path={ROUTES.NEWUSER} component={NewUser} />
 
     </div>
--- src/Components/TestHome/home.js
+++ src/Components/TestHome/home.js
@@ -3,46 +3,121 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import {LoremIpsum} from 'react-lorem-ipsum';
 import * as ROUTES from '../../Constants/routes';
 import Nav from './Navbar/Nav';
+import { SocialIcon } from 'react-social-icons';
 
+// import images
+import logo from "../../assets/images/logo.png";
+import kathak_left from "../../assets/images/main/4_left.png";
+import kathak_right from "../../assets/images/main/4_right.png";
+
+// videos
+import vid from "../../assets/videos/aftermovie_cut.mp4";
+
+import "../../Styles/_main.scss";
 import "./home.scss"
 
 const Home = () => (
-    <div>
-      <Nav />
-      <div className="clear-nav container">
-        <div id='Home'>
-          <h1 className="main-headers">
-          Home
-          </h1>
+    <main>
+      <section className="section-wrapper">
+        <section className="section section-home">
+          <div className="header">
+              <img className="header__logo" src={logo} />
+          </div>
+          <div className="tagline">
+            <h1 className="tagline_heading-primary">
+              <span className="tagline_heading-primary--main">Embracing the</span>
+              <span className="tagline_heading-primary--sub">curry culture</span>
+            </h1>
+          </div>
+        </section>
+        <section className="section section-about">
+          <div className="about">
+            <h2 className="about__header">About</h2>
+            <p className="about__description">
+              Felicity is iiit's annual culture and tech fest. Held every year at he beginning of the 
+              spring semester, it is the highlight of an iiitian's year and is an event that everybody
+              looks forward to. Felicity encompasses and embraces the varied and diverse interests of 
+              iiit with everyone playing a role.
+            </p>
+          </div>
+        </section>
+        <section className="section section-theme">
+          <div className="section-theme__text">
+            <span>Indian</span>
+            <span>Culture</span>
+          </div>
+        </section>
+      </section>
+      <section className="section section-testimonials">
+        
+        <div className="testimonials">
+          <img src={kathak_left} className="kathak__left"/>
+          <img src={kathak_right} className="kathak__right"/>
+          <div className="testimonials-text">
+            <h2 className="testimonials-text__header">
+              Testimonials
+            </h2>
+            <div className="custom-row">
+              <div className="col-1-of-3 testimonial-card">
+                  <span className="testimonial-card-quote">&ldquo;To love is to hate, to fear is to conquer, and other bullshit you can tell yourself&rdquo;</span>
+                  <span className="testimonial-card-author">-Shaunak Badani</span>
+              </div>
+              <div className="col-1-of-3 testimonial-card">
+                  <span className="testimonial-card-quote">&ldquo;To love is to hate, to fear is to conquer, and other bullshit you can tell yourself&rdquo;</span>
+                  <span className="testimonial-card-author">-Shaunak Badani</span>
+              </div>
+              <div className="col-1-of-3 testimonial-card">
+                  <span className="testimonial-card-quote">&ldquo;To love is to hate, to fear is to conquer, and other bullshit you can tell yourself"</span>
+                  <span className="testimonial-card-author">-Shaunak Badani</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <LoremIpsum p="100"> </LoremIpsum>
-        <div id='About'>
-          <h1 className="main-headers">
-          About
-          </h1>
+      </section>
+      <section className="section section-footer">
+        <div className="bg-video">
+            <video className="bg-video__content" autoPlay muted loop>
+                <source src={vid} type="video/webm" /> 
+                Your browser is not supported
+            </video>
         </div>
-        <LoremIpsum p="100"> </LoremIpsum>
-        <div id='Theme'>
-          <h1 className="main-headers">
-          Theme
-          </h1>
-        </div>
-        <LoremIpsum p="100"> </LoremIpsum>
-        <div id='Testimonials'>
-          <h1 className="main-headers">
-          Testimonials
-          </h1>
-        </div>
-        <LoremIpsum p="100"> </LoremIpsum>
-        <div id='Video'>
-          <h1 className="main-headers">
-          Video
-          </h1>
-        </div>
-        <LoremIpsum p="100"> </LoremIpsum>
-      </div>
+        <footer>
+          <div className="footer">
+            <div className="footer__text">
+                <p>Connect with us : </p>
+                <div>
+                  <SocialIcon 
+                   network="instagram"
+                   url="https://instagram.com"
+                   className="footer__icon" 
+                   bgColor="transparent"
+                   fgColor="white" />
+                  <SocialIcon 
+                   network="facebook"
+                   url="https://instagram.com"
+                   className="footer__icon" 
+                   bgColor="transparent"
+                   fgColor="white" />
+                  <SocialIcon 
+                   network="youtube"
+                   url="https://instagram.com"
+                   className="footer__icon" 
+                   bgColor="transparent"
+                   fgColor="white" />
+                  <SocialIcon 
+                   network="twitter"
+                   url="https://instagram.com"
+                   className="footer__icon" 
+                   bgColor="transparent"
+                   fgColor="white" />
+                </div>
 
-    </div>
+            </div>
+          </div>
+        </footer>
+      </section>
+
+    </main>
 
   );
 
